823e22a748e9e3ea487e45981675a1ed4c8eaeb5
Dave <dave.van.den.berg@student.nhlstenden.com>
2024-02-14 09:53:20 +0100

    Changed ServoControl.ino into seperate functions for every servo rotation instead of test variant

--- battleBot/ServoControl.ino
+++ battleBot/ServoControl.ino
@@ -1,35 +1,21 @@
-void servoControl() {
-  // I need to fix this LOL
-  digitalWrite(echoServo, LOW);
-  delay(1500);
-  // look forward
+void servoForward(){
+  moveGripper(1500);
+}
+
+void servoLeft(){
+  moveGripper(2500);
+}
+
+void servoRight(){
+  moveGripper(500);
+}
+
+void moveGripper(int pulseDuration){
   for(int i = 0; i < 10; i++)
   {
     delay(20);
     digitalWrite(echoServo, HIGH);
-    delayMicroseconds(1500);
-    digitalWrite(echoServo, LOW);
-  }
-  digitalWrite(echoServo, LOW);
-  delay(1500);
-  
-  // look left
-  for(int i = 0; i < 10; i++)
-  {
-    delay(20);
-    digitalWrite(echoServo, HIGH);
-    delayMicroseconds(2500);
-    digitalWrite(echoServo, LOW);
-  }
-  digitalWrite(echoServo, LOW);
-  delay(1500);
-  
-  // look right
-  for(int i = 0; i < 10; i++)
-  {
-    delay(20);
-    digitalWrite(echoServo, HIGH);
-    delayMicroseconds(500);
+    delayMicroseconds(pulseDuration);
     digitalWrite(echoServo, LOW);
   }
 }
